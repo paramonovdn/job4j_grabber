5d5e935d787c1c4063a5c807d83cb53bf3c2f0b0
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-05-30 14:13:53 +0300

    6. Grabber [#289477]

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,84 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Parse parse;
+    private final Store store;
+    private final Scheduler scheduler;
+    private final int time;
+
+    private static final String PAGE_LINK = "https://career.habr.com/vacancies/java_developer?page=";
+
+    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
+        this.parse = parse;
+        this.store = store;
+        this.scheduler = scheduler;
+        this.time = time;
+    }
+
+    @Override
+    public void init() throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(time)
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            List<Post> postList = new ArrayList<>();
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    postList.addAll(parse.list(PAGE_LINK + i));
+                }
+                for (Post post : postList) {
+                    store.save(post);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        var cfg = new Properties();
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        var parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        var store = new PsqlStore(cfg);
+        var time = Integer.parseInt(cfg.getProperty("time"));
+        new Grabber(parse, store, scheduler, time).init();
+    }
+}
